b6b93c027c9e87de7e3de80b9b3077333123b349
xAlessandroo <alessandro.iozzino1@gmail.com>
2023-12-14 16:24:08 +0100

    component2

---
+++ comp2/src/App.jsx
@@ -0,0 +1,11 @@
+import { Hello } from "./HelloWorld";
+import { Message } from "./Message";
+
+export function App(){
+    return (
+    <div>
+        <Hello />
+        <Message />
+    </div>
+    )
+}
