1afaed98a5cf22b7f4cc96f3ea506c8023b74a3e
xAlessandroo <alessandro.iozzino1@gmail.com>
2023-12-18 17:43:13 +0100

    handleEvents2

--- my-app/src/MouseClicker.jsx
+++ my-app/src/MouseClicker.jsx
@@ -1,10 +1,16 @@
-export function MouseClicker(){
-
-    function handleClick(e){
+export function MouseClicker() {
+  function handleClick(e) {
+    if (e.target.tagName.toLowerCase() === "img"){
+        console.log(e.target.src)
+    } else {
         console.log(e.target.name);
     }
+  }
 
-    return (
-        <button name="one" onClick={handleClick}>Click me!</button>
-    )
+  return (
+    <button name="one" onClick={handleClick}>
+      <img src="/vite.svg" width={30} height={30} />
+      Click me!
+    </button>
+  );
 }
