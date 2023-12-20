8b5b6200c704b02197d2e47ded853dc177b2ee72
xAlessandroo <alessandro.iozzino1@gmail.com>
2023-12-20 18:20:40 +0100

    refs2

--- my-app/src/FocusableInput.jsx
+++ my-app/src/FocusableInput.jsx
@@ -2,9 +2,17 @@ import { useEffect, useRef } from "react"
 
 export function FocusableInput(){
 
+    const mountedRef = useRef(false)
     const inputRef = useRef(null)
 
     useEffect(() => {
+        if(!mountedRef.current){
+            mountedRef.current = true
+            console.log('Mounting for the first time');
+        } else {
+            console.log('Mounting for debug');
+        }
+
         inputRef.current.focus()
     })
 
