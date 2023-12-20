e1e192807fa0c97196f4bfd39472c8d3d6d0646d
xAlessandroo <alessandro.iozzino1@gmail.com>
2023-12-20 16:10:45 +0100

    uncontrolledForm2

--- my-app/src/UncontrolledLogin.jsx
+++ my-app/src/UncontrolledLogin.jsx
@@ -1,32 +1,38 @@
-export function UncontrolledLogin({ onLogin}){
+export function UncontrolledLogin({ onLogin }) {
+  function handleLogin(event) {
+    event.preventDefault();
 
-    function handleLogin(event){
-        event.preventDefault();
+    const formData = new FormData(event.target)
+    const username = formData.get('username')
+    const password = formData.get('password')
+    const remember = formData.get('remember')
 
-        const username = event.target.username.value;
-        const password = event.target.password.value;
-        const remember = event.target.remember.checked;
+    onLogin({
+      username,
+      password,
+      remember,
+    });
+  }
 
-        onLogin({
-            username,
-            password,
-            remember,
-        });
-    }
+  function handleReset(event) {
+    event.target.reset();
+  }
 
-    function handleReset(event){
-        event.target.reset();
-    }
+  return (
+    <form onSubmit={handleLogin} onReset={handleReset}>
+      <input type="text" name="username" placeholder="username" />
+      <input type="password" name="password" placeholder="password" />
+      <label htmlFor="remember">Remember for later</label>
+      <input type="checkbox" name="remember" />
 
-    return (
-        <form onSubmit={handleLogin} onReset={handleReset}>
-            <input type="text" name="username" placeholder="username"/>
-            <input type="password" name="password" placeholder="password"/>
-            <label htmlFor="remember">Remember for later</label>
-            <input type="checkbox" name="remember" />
-
-            <button type="submit">Submit</button>
-            <button type="reset">Reset</button>
-        </form>
-    )
+      <button type="submit">Submit</button>
+      <button type="reset">Reset</button>
+    </form>
+  );
 }
+// Vantaggi
+// Con formData abbiamo più flessibilità con gli input/elementi
+
+// Svantaggi
+// Non tutti i browser supportano formData e quindi è più limitato, inoltre alcuni browser potrebbero trattare gli elementi ed i valori in modo diverso. 
+// Infine non si ha il controllo manuale dei dati, potrebbero accadare quindi azioni indesiderate 
