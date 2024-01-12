2172bdfb4c33650019ac7dcdbc46dd6ee1d394d9
xAlessandroo <alessandro.iozzino1@gmail.com>
2024-01-12 16:48:29 +0100

    route2

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,13 +1,16 @@
 import { Counter } from "./Counter";
 import { Welcome } from "./Welcome";
+import {Routes, Route} from 'react-router-dom'
 
 export function App(){
     return (
         <div>
             <h1>My App</h1>
             <hr />
-            <Welcome name="Alex" age={22}/>
-            <Counter />
+            <Routes>
+                <Route path="/" element={<Welcome name="Alex" age={22}/>}/>
+                <Route path=":counter" element={<Counter />} />
+            </Routes>
         </div>
     )
 }
--- my-app/src/index.jsx
+++ my-app/src/index.jsx
@@ -1,9 +1,9 @@
-import { StrictMode } from "react"
+import { BrowserRouter } from 'react-router-dom'
 import { App } from "./App"
 import { createRoot } from 'react-dom/client'
 
 createRoot(document.getElementById('root')).render(
-    <StrictMode>
+    <BrowserRouter>
         <App />
-    </StrictMode>
+    </BrowserRouter>
 )
