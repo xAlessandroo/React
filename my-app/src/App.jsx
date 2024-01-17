16a7dd3a4be11331712ccb5c29fa458dc178bf4a
xAlessandroo <alessandro.iozzino1@gmail.com>
2024-01-17 10:28:54 +0100

    mod

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,19 +1,9 @@
-import { Routes, Route, Link} from 'react-router-dom'
-import { Container } from "./Container";
-import { Welcome } from './Welcome'
-import { ShowGithubUser } from './ShowGithubUser';
-import { Counter } from './Counter'
+import { Form } from "./Form";
 
-export function App() {
-
-  return (
-    <Container title='My App'>
-      <Routes>
-        <Route path='/' element={<Welcome />}></Route>
-        <Route path='counter' element={<Counter />} />
-        <Route path='/:username' element={<ShowGithubUser />}></Route>
-      </Routes>
-      <Link to='/'>Home</Link> | <Link to='counter'>Counter</Link> | <Link to='xAlessandroo'>Github User xAlessandroo</Link>
-    </Container>
-  );
+export function App(){
+    return (
+        <div>
+            <Form />
+        </div>
+    )
 }
