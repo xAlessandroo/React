1e26b3cf920d1e83fbc7b1b3439bbe96995def82
xAlessandroo <alessandro.iozzino1@gmail.com>
2024-01-17 09:38:36 +0100

    route5

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,19 +1,27 @@
-import { Routes, Route, Link} from 'react-router-dom'
+import { Routes, Route, Link } from "react-router-dom";
 import { Container } from "./Container";
-import { Welcome } from './Welcome'
-import { ShowGithubUser } from './ShowGithubUser';
-import { Counter } from './Counter'
+import { Welcome } from "./Welcome";
+import { ShowGithubUser } from "./ShowGithubUser";
+import { Counter } from "./Counter";
 
 export function App() {
-
   return (
-    <Container title='My App'>
-      <Routes>
-        <Route path='/' element={<Welcome />}></Route>
-        <Route path='counter' element={<Counter />} />
-        <Route path='/:username' element={<ShowGithubUser />}></Route>
+    <Container title="My App">
+      <Routes>   
+        <Route
+          path="*"
+          element={
+            <div>
+              <p>Page not found!</p>
+              <Link to="/">Home</Link>
+            </div>
+          }
+        />
+        <Route path="/" element={<Welcome />}></Route>
+        <Route path="counter" element={<Counter />} />
+        <Route path="/:username" element={<ShowGithubUser />}></Route>
+     
       </Routes>
-      <Link to='/'>Home</Link> | <Link to='counter'>Counter</Link> | <Link to='xAlessandroo'>Github User xAlessandroo</Link>
     </Container>
   );
 }
