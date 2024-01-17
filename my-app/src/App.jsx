d2f457b51ac60031331762938c889e7af64089a7
xAlessandroo <alessandro.iozzino1@gmail.com>
2024-01-17 17:23:01 +0100

    swr4

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,13 +1,18 @@
 import { Container } from "./Container";
 import { Welcome } from "./Welcome";
 import { GithubUser } from "./GithubUser";
+import { SWRConfig } from "swr";
+
+const fetcher = url => fetch(url).then(response=> response.json())
 
 export function App() {
 
   return (
     <Container title={<h1>My app!</h1>}>
       <Welcome name="Alex" />
+      <SWRConfig value={{fetcher}}>
       <GithubUser username='xAlessandroo'/>
+      </SWRConfig>
     </Container>
   );
 }
--- my-app/src/useGithubUser.jsx
+++ my-app/src/useGithubUser.jsx
@@ -1,10 +1,8 @@
 import useSWR from "swr";
 
-const fetcher = url => fetch(url).then(response=> response.json())
-
 export function useGithubUser(username) {
 
-  const { data, error, mutate } = useSWR(username ? `https://api.github.com/users/${username}`: null, fetcher)
+  const { data, error, mutate } = useSWR(username ? `https://api.github.com/users/${username}`: null)
  
   function fetchGithubUser(){
     mutate()
