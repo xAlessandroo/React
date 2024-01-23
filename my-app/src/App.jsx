95873a624a859ae64fd2a3d55e4773fe133d253f
xAlessandroo <alessandro.iozzino1@gmail.com>
2024-01-23 10:36:26 +0100

    preparazione ide

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,16 +1,7 @@
-import { Route, Routes } from "react-router-dom"
-import FetchNameF1 from "./FetchNameF1"
-import ShowF1Details from "./ShowF1Details"
-import { ButtonLogin } from "./ButtonLogin"
 
 export function App() {
 
   return (
-    <Routes>
-      <Route path="/" element={<ButtonLogin />}/>
-      <Route path='/el' element={<FetchNameF1/>} />
-      <Route path="/el/:pos"element={<ShowF1Details/>}/>
-    
-    </Routes>
+    <div></div>
   )
 }
--- my-app/src/Color.jsx
+++
@@ -1,3 +0,0 @@
-export function Color({ color }){
-    return <li>{color.name}</li>
-}
